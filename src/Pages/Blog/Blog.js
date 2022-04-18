fc1c1f20b5ac1bdc2dbe963000c3135622c506e4
minhaz2597 <md.minhazuddin2597@gmail.com>
2022-04-18 23:44:23 +0600

    modify  again

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -1,5 +1,4 @@
 import React from "react";
-import blog1 from "../../images/blog1.jpg";
 import blog2 from "../../images/blog2.jpg";
 import "./Blog.css";
 
@@ -8,22 +7,51 @@ const Blog = () => {
     <div>
       <div className="blog-container">
         <div className="blog-text">
-          <h1> Fit Bottomed Girls </h1>
+          <h1> Authorization vs Authentication</h1>
           <p>
-            The Fit Bottomed world is run by fitness professionals Jennipher
-            Walters and Kristen Seymour. The two friends started Fit Bottomed
-            Girls in 2008 as they were looking for a fresh approach to health
-            and fitness without the dieting. The blog has been growing ever
-            since and has now expanded into other verticals such as Fit Bottomed
-            Mammas, the Fit Bottomed Girls Podcast, and the online course Love
-            Your Body. Love Your Life. The blog was launched as a way to solve a
-            problem; they were looking for a new way to be healthy. Just months
-            after starting, readers latched on and related to the content almost
-            immediately.
+            Authorization in system security is the process of giving the user
+            permission to access a specific resource or function. This term is
+            often used interchangeably with access control or client privilege.
+            <h2>Authentication</h2>
+            Authentication is the act of validating that users are whom they
+            claim to be. This is the first step in any security process.
+            Complete an authentication process with: Passwords. Usernames and
+            passwords are the most common authentication factors. If a user
+            enters the correct data, the system assumes the identity is valid
+            and grants access. One-time pins. Grant access for only one session
+            or transaction. Authentication apps. Generate security codes via an
+            outside party that grants access.
           </p>
-        </div>
-        <div>
-          <img className="blog1-img" src={blog1} alt="" />
+          <h1>
+            Why are you using firebase? What other options do you have to
+            implement authentication?
+          </h1>
+          <p>
+            Firebase by Google can be used for the following: Firebase manages
+            all data real-time in the database. So, the exchange of data to and
+            fro from the database is easy and quick. Hence, if you are looking
+            to develop mobile apps such as live streaming, chat messaging, etc.,
+            you can use Firebase. Usually, authentication by a server entails
+            the use of a user name and password. Other ways to authenticate can
+            be through cards, retina scans, voice recognition, and fingerprints.
+            Authentication by a client usually involves the server giving a
+            certificate to the client in which a trusted third party such as
+            Verisign or Thawte states that the server belongs to the entity
+            (such as a bank) that the client expects it to.
+          </p>
+          <h1>
+            What other services does firebase provide other than authentication
+          </h1>
+          <p>
+            Firebase Authentication provides backend services, easy-to-use SDKs,
+            and ready-made UI libraries to authenticate users to your app. It
+            supports authentication using passwords, phone numbers, popular
+            federated identity providers like Google, Facebook and Twitter, and
+            more. Firebase Authentication integrates tightly with other Firebase
+            services, and it leverages industry standards like OAuth 2.0 and
+            OpenID Connect, so it can be easily integrated with your custom
+            backend.
+          </p>
         </div>
       </div>
       <div className="blog-container">
